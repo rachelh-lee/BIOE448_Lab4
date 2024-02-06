985b87c23352dbfa17d029511b09121a7dd8b5a7
rachelh-lee <hl92@rice.edu>
2024-02-06 15:56:20 -0600

    Red led lights when distance is greater than threshold

--- sensorsandcalibration/sensorsandcalibration.ino
+++ sensorsandcalibration/sensorsandcalibration.ino
@@ -12,6 +12,11 @@ const double R1 = 0.0343/2;
 // speed of sound in cm/uS and divded by two because the sound travels there and back
 const double R2 = 0.0135/2;
 // speed of sound in In/uS and divded by two because the sound travels there and back
+const int LEDPin = 6;
+// setting pin6 to input to Red Led
+const int LEDPin2 = 2;
+// setting pin 2 to input to Green Led 
+const int threshold = 3;
 
 void setup() {
   // put your setup code here, to run once:
@@ -37,4 +42,14 @@ void loop() {
   Serial.print(distanceInch);
   Serial.println(" in");
   delay(1000);
+  // adding complexity, if distance is less than threshold, Green LED lights up, if not Red lED lights up
+  if (distanceInch > threshold) {
+    digitalWrite(LEDPin, HIGH);
+    digitalWrite(LEDPin2, LOW);
+  }
+  else {
+    digitalWrite(LEDPin, LOW);
+    digitalWrite(LEDPin2, HIGH);
+  }
+
 }
