73ec981f92ae312a806b0f30a85e14e09176249f
rachelh-lee <hl92@rice.edu>
2024-02-06 15:02:00 -0600

    declared variables of interest

--- sensorsandcalibration/sensorsandcalibration.ino
+++ sensorsandcalibration/sensorsandcalibration.ino
@@ -1,3 +1,14 @@
+
+// trig is to pin 11
+// echo is to pin 12
+// Vcc is to 5V
+// GND is to GND
+
+const int trigPin = 11;
+const int echoPin = 12;
+long duration;
+int distanceCm, distanceInch;
+
 void setup() {
   // put your setup code here, to run once:
 
